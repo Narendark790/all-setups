1a7db5ea52406c658b53c3f0194aee75b9890f78
Narendark790 <koletinarendar7@gmail.com>
2026-03-24 13:12:28 +0530

    Change package manager commands for Ubuntu

--- 1.amazon.sh
+++ 1.amazon.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo -i
-yum update
-yum install httpd git -y
+apt update
+apt install apache2 -y
 git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
 mv online-clone-amazon/* /var/www/html
